e0da2061d492a3bd74177192b21711ee3fd2d62f
Mario Giordano <164010608+LesPaul997@users.noreply.github.com>
2024-09-28 20:37:52 +0200

    Initial commit

---
+++ src/test/java/org/hello/spring/mvc/TicketPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hello.spring.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
